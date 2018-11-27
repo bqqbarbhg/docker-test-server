55985b3128f2e95356c53fa835db269e446aaa52
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-27 17:34:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello from ' + process.env.MESSAGE))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
